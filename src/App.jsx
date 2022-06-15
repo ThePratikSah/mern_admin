7d7b4733562deeeea5e2c985928febafc02fd6c3
Pratik Sah <shivam3448@gmail.com>
2022-06-15 14:07:45 +0530

    added new function to add new user

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import React, {useState, useEffect} from "react";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
-import UserContext from "./context/UserContext";
-import Home from "./component/Home/Home";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./component/Header";
+import Home from "./component/Home/Home";
+import UserContext from "./context/UserContext";
 
 function App() {
-  
   const [user, setUser] = useState({
     isAuth: false,
     token: undefined,
     user: undefined,
-    expiryTime: undefined
+    expiryTime: undefined,
   });
 
   useEffect(() => {
@@ -20,7 +19,7 @@ function App() {
       let token = localStorage.getItem("auth-token");
       //check for the expiredTime in localStorage
       let expTime = localStorage.getItem("exp-time");
-      if (token === null || expTime === null || expTime==="") {
+      if (token === null || expTime === null || expTime === "") {
         //if no token key found, set it to an empty string
         localStorage.setItem("auth-token", "");
         localStorage.setItem("exp-time", "");
@@ -28,23 +27,28 @@ function App() {
         expTime = "";
         return;
       }
-    
+
+      // this is the new code I've added
+      function addNewUser() {
+        return "new user added!";
+      }
+
       let currentTime = new Date().toISOString();
       let expiryTime = new Date(expTime).toISOString();
-      if ((expiryTime > currentTime) && token) {
-        setUser(user => ({
+      if (expiryTime > currentTime && token) {
+        setUser((user) => ({
           ...user,
           isAuth: true,
           token,
-          expiryTime
+          expiryTime,
         }));
       } else {
         token = "";
       }
-    }
+    };
     autoLogin();
   }, []);
-  
+
   //logic for logout
   const logOut = () => {
     setUser({
@@ -52,25 +56,25 @@ function App() {
       token: undefined,
       user: undefined,
       isAuth: false,
-      expiryTime: undefined
+      expiryTime: undefined,
     });
-    
+
     //removing the token from localStorage
     const token = localStorage.getItem("auth-token");
     if (token) {
       localStorage.setItem("auth-token", "");
       localStorage.setItem("exp-time", "");
     }
-  }
-  
+  };
+
   return (
     <>
       <BrowserRouter>
-        <UserContext.Provider value={{user, setUser, logOut}}>
+        <UserContext.Provider value={{ user, setUser, logOut }}>
           <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
         </UserContext.Provider>
       </BrowserRouter>
     </>
